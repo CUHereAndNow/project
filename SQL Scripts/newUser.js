1851fd3161245db7588da61161da9fa33a383d7f
Jorge Moreno <jomo2834@colorado.edu>
2018-04-04 21:02:05 -0600

    fixed a mistake

--- SQL Scripts/newUser.js
+++ SQL Scripts/newUser.js
@@ -1,7 +1,12 @@
 var mysql = require('mysql');
-var express = require('express')
+var express = require('express');
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
+
+//Need this in order to get the info from our form
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
 app.get('/newUser.css',function(req, res){
 	res.sendFile(__dirname + "/" + "newUser.css");	
@@ -16,9 +21,10 @@ app.get('/', function (req,res) {
 // This code handles the HTML form sent via GET on Submit
 // This is getting the number the user inputs in the form
 app.post('/process_post', function (req, res) {
-	var name = req.query.uname;
-	var pass = req.query.psw;
-	
+	 name = req.body.uname;
+	 pass = req.body.psw;
+	 
+	 
 	// This code sets up DB connection variables
 	var con = mysql.createConnection({
 		host: "localhost",
@@ -50,6 +56,7 @@ app.post('/process_post', function (req, res) {
 			{
 				//If it doest not exist it adds it to the database here.
 				//console.log("not the same");
+
 				con.query("Insert into UserProfiles (`Email_Address` , `Passwords`) Values ('" + name +  "','" + pass + "');", function (err, result) {
 					if (err) throw err;
 					res.send("1 record inserted");
